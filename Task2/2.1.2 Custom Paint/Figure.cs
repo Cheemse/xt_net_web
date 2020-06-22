b65f38a51f2177020e806feb07f6eea64d3dbafe
Kirill <guskir259@gmail.com>
2020-06-22 18:21:33 +0400

    Final version

---
+++ Task2/2.1.2 Custom Paint/Figure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._2_Custom_Paint
+{
+    public abstract class Figure : IFigure
+    {
+        public abstract string ShowInfo();
+
+        public abstract double Length();
+    }
+}
